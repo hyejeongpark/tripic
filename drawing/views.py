959fd207e1ac9061353348d40a16592dab51793a
Hyejeong Park <phj1698@gmail.com>
2016-04-03 10:12:24 +0900

    Make index to show post

--- drawing/views.py
+++ drawing/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return render(request, 'drawing/index.html')
+    post_list = Post.objects.all()
+    return render(request, 'drawing/index.html', {'post_list': post_list,})
