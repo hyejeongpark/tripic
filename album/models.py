584965c4f30590d2013d4880f183e7d65083afc3
Hyejeong Park <phj1698@gmail.com>
2016-06-08 19:30:28 +0900

    Reset

--- album/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    photo = models.ImageField()
-    content = models.CharField(max_length=100, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
--- album/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'album/index.html', {'post_list': post_list,})
