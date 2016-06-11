c538f78d75efa4e8a8be94a968ccbd73d41a4a91
Hyejeong Park <phj1698@gmail.com>
2016-06-11 22:50:58 +0900

    Add comment input

--- aalborg/aalborg.js
+++ aalborg/aalborg.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-  $('.heart').click(function(){
+$(document).ready(function() {
+  $('.heart').click(function() {
       $(this).toggleClass('heartAnimation');
     });
+
+  $('input').keypress(function(e) {
+    var value = $(this).val();
+    if(e.which == 13) {
+      $('.display-none').removeClass('display-none');
+      $('span').text(value);
+      $(this).val("");
+    }
+  })
 });
+
